9744661d1fbbd0de44aa9dc6aa3c10058f589a30
dysshanks <ryanvdvorst@outlook.com>
2025-01-29 08:57:21 -0800

    eerste commit

---
+++ config/config.example.php
@@ -0,0 +1,11 @@
+<?php
+// ieder voor zich de echte config staat als gitignore
+// dont delete
+// maak je eige en hernoem hem naar "config.php"
+
+$dbhost = "localhost"; //kan ook iets anders zijn zoals ip bijvoorbeeld: (127.0.0.1)
+$dbname = ""; //required
+$dbuser = "root"; // requred kan ook anders zijn
+$dbpass = ""; // nullable
+
+$base_url = "http://todolist.test"; // eindig nooit op een /
