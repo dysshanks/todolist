30284971d8adb774f210f22ba0dae8470aa3cfa1
dysshanks <ryanvdvorst@outlook.com>
2025-01-29 10:44:29 -0800

    comment

--- recources/views/components/head.php
+++ recources/views/components/head.php
@@ -9,5 +9,5 @@ require_once __DIR__."/../../../config/config.php";
 <meta name="description" content="todo app gemaakt als persoonlijk project">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="icon" href="<?php echo $base_url;?>/public_html/img/icons/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo $base_url;?>/public_html/img/icons/favicon.ico" type="image/x-icon">  <!--de top icoon-->
 <link rel="stylesheet" href="<?php echo $base_url;?>/public_html/css/style.css">
